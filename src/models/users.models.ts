6eccee3c51adae3ef5a89287b2d66ab638a7cd50
Leonaro-L-Cunha <leonavidareal@gmail.com>
2023-08-23 11:08:23 -0300

    feat: user model

---
+++ src/models/users.models.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import { UserDTO } from '../interfaces/user.interface';
+
+const userSchema = new mongoose.Schema<UserDTO>({
+  id: { type: String },
+  name: { type: String, required: true },
+  email: { type: String, unique: true },
+  password: { type: String, required: true, select: false },
+});
+
+const Users = mongoose.model('users', userSchema);
+
+export default Users;
