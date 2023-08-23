5e02a6b255e72e84d5f4500ce4c3a4eb28531da2
Leonaro-L-Cunha <leonavidareal@gmail.com>
2023-08-23 11:08:44 -0300

    feat: user routes

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import authorsRoutes from './routers/authors.routes';
 import 'express-async-errors';
 import { handleErros } from './middleware/handleErros';
 import { notFoundPage } from './middleware/notFoundPage';
+import userRoutes from './routers/users.routes';
 
 const app: Application = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use('/books', booksRoutes);
 app.use('/authors', authorsRoutes);
+app.use('/users', userRoutes);
 
 app.use(notFoundPage);
 
---
+++ src/routers/users.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+const userRoutes: Router = Router();
+
+userRoutes.post('');
+userRoutes.get('');
+userRoutes.get('/:id');
+userRoutes.put('/:id');
+userRoutes.delete('/:id');
+
+export default userRoutes;
