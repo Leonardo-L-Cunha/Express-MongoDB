5c275304cd1d3666918692e8610f2ddd86fe6ef7
Leonaro-L-Cunha <leonavidareal@gmail.com>
2023-10-12 11:57:29 -0300

    adjust: route

--- src/routers/users.routes.ts
+++ src/routers/users.routes.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express';
 import { UsersController } from '../controller/users.controller';
 import { verifyToken } from '../middleware/auth';
+import { verifyIfAdmin } from '../middleware/verifyIfAdmin';
 
 const userRoutes: Router = Router();
 
 userRoutes.post('', UsersController.createUser);
 userRoutes.get('', UsersController.ListUsers);
 userRoutes.get('/:id', UsersController.ListOneUser);
-userRoutes.put('/:id', verifyToken, UsersController.updatedUser);
-userRoutes.delete('/:id', verifyToken, UsersController.deletedUser);
+userRoutes.put('/:id', verifyToken, verifyIfAdmin, UsersController.updatedUser);
+userRoutes.delete(
+  '/:id',
+  verifyToken,
+  verifyIfAdmin,
+  UsersController.deletedUser
+);
 
 export default userRoutes;
